d5ee71612230a413f304d0d7549d45def35bfcc2
HungNH0805 <hung.nguyenhuy0805@gmail.com>
2022-04-26 12:08:02 +0700

    synchro code

--- Tubepxuyenviet/.history/template/views-view-unformatted--front-block--block-mau-chuyen-front.tpl_20220317090822.php
+++
@@ -1,9 +0,0 @@
-<div class="story mb-30">
-  <div class="container">
-    <div class=" item-story ">
-      <?php foreach ($rows as $row) : ?>
-        <?php print $row ?>
-      <?php endforeach; ?>
-    </div>
-  </div>
-</div>
